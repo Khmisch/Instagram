916c343cdea1da93b86dd3a0af2df322d4b796a8
Mukaddam Khamraeva <khamraeva.m1124@gmail.com>
2022-02-18 17:21:23 +0500

    Initial commit

---
+++ app/src/main/java/com/example/instagram/activity/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.instagram.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.facebook.adapter.FeedAdapter
+import com.example.facebook.model.Feed
+import com.example.facebook.model.Post
+import com.example.facebook.model.Story
+import com.example.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.setLayoutManager(GridLayoutManager(this, 1))
+
+        refreshFeedAdapter(getAllFeeds())
+    }
+
+
+    private fun refreshFeedAdapter(allFeeds: ArrayList<Feed>) {
+        val adapter = FeedAdapter(this, allFeeds)
+        recyclerView!!.adapter = adapter
+    }
+
+    private fun getAllFeeds(): ArrayList<Feed> {
+        val stories: ArrayList<Story> = ArrayList<Story>()
+        stories.add(Story(R.drawable.im_user_4, "Xurshid"))
+        stories.add(Story(R.drawable.im_user_3, "Sherzod"))
+        stories.add(Story(R.drawable.im_user_2, "Begzod"))
+        stories.add(Story(R.drawable.im_user_1, "Firdavs"))
+        stories.add(Story(R.drawable.im_user_4, "Xurshid"))
+        stories.add(Story(R.drawable.im_user_3, "Sherzod"))
+        stories.add(Story(R.drawable.im_user_2, "Begzod"))
+        stories.add(Story(R.drawable.im_user_1, "Firdavs"))
+
+        val feeds: ArrayList<Feed> = ArrayList<Feed>()
+        feeds.add(Feed(stories))
+        feeds.add(Feed(Post(R.drawable.im_user_1, "Xurshidbek", R.drawable.im_post_4)))
+        feeds.add(Feed(Post(R.drawable.im_user_2, "Begzodbek", R.drawable.im_post_3)))
+        feeds.add(Feed(Post(R.drawable.im_user_3, "Sherzodbek", R.drawable.im_post_2)))
+        feeds.add(Feed(Post(R.drawable.im_user_4, "Firdavsbek", R.drawable.im_post_1)))
+
+        return feeds;
+    }
+}
---
+++ app/src/main/java/com/example/instagram/model/Feed.kt
@@ -0,0 +1,17 @@
+package com.example.facebook.model
+
+class Feed {
+    var post: Post? = null
+    var stories:ArrayList<Story> = ArrayList<Story>()
+
+
+    constructor(){
+    }
+
+    constructor(post: Post){
+        this.post= post
+    }
+    constructor(stories: ArrayList<Story>){
+        this.stories = stories
+    }
+}
